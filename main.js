e463cbe13ac9de4269bd48b49200b5240a280a0f
Supriyo Mondal <hisupriyooo@gmail.com>
2019-10-24 20:05:05 +0530

    'basic'

--- main.js
+++ main.js
@@ -53,16 +53,20 @@ function showTodo() {
 }
 function deleteTodo(elm) {
   //console.log(elm.parentElement.parentElement.remove());
-  const named = elm.parentElement.previousElementSibling.textContent;
-  console.log(named);
+  const description = elm.parentElement.previousElementSibling.textContent;
 
-  const todoList = localStorage.getItem("todoList");
-
-  todoList.filter(todo => todo.description != named);
-
-  showTodo();
+  const todoList = JSON.parse(localStorage.getItem("todoList"));
+  todoList.forEach((todo, index) => {
+    if (todo.description === description) {
+      todoList.splice(index, 1);
+    }
+  });
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
 
 document.addEventListener("DOMContentLoaded", showTodo);
 document.querySelector("#todoform").addEventListener("submit", formSubmit);
-tablebody.addEventListener("click", e => deleteTodo(e.target));
+tablebody.addEventListener("click", e => {
+  deleteTodo(e.target);
+  showTodo();
+});
